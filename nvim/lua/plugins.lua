c6ab358c1636f8c6ccb32d2add65817d042828bf
denesbeck <denes.beck1@outlook.com>
2025-10-14 20:01:46 +0200

    feat: copilot chat

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,129 +3,142 @@ local vim = vim
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	-- LSP
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+  -- LSP
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
 
-	-- LSP Saga
-	"glepnir/lspsaga.nvim",
+  -- LSP Saga
+  "glepnir/lspsaga.nvim",
 
-	-- Theme
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- Theme
+  { "catppuccin/nvim",              name = "catppuccin", priority = 1000 },
 
-	-- Devicons
-	"nvim-tree/nvim-web-devicons",
+  -- Devicons
+  "nvim-tree/nvim-web-devicons",
 
-	-- Status bar
-	"nvim-lualine/lualine.nvim",
+  -- Status bar
+  "nvim-lualine/lualine.nvim",
 
-	-- Fuzzy finder
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- Fuzzy finder
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" } },
-	},
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" } },
+  },
 
-	-- Git
-	"lewis6991/gitsigns.nvim",
+  -- Git
+  "lewis6991/gitsigns.nvim",
 
-	-- Syntax highlight
-	"nvim-treesitter/nvim-treesitter",
+  -- Syntax highlight
+  "nvim-treesitter/nvim-treesitter",
 
-	-- Autoclose
-	"windwp/nvim-ts-autotag",
-	"windwp/nvim-autopairs",
+  -- Autoclose
+  "windwp/nvim-ts-autotag",
+  "windwp/nvim-autopairs",
 
-	-- Autocomplete
-	-- Tailwind CSS
-	"js-everts/cmp-tailwind-colors",
+  -- Autocomplete
+  -- Tailwind CSS
+  "js-everts/cmp-tailwind-colors",
 
-	-- CMP
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/cmp-vsnip",
-	"hrsh7th/vim-vsnip",
+  -- CMP
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/cmp-vsnip",
+  "hrsh7th/vim-vsnip",
 
-	-- IncRename
-	{
-		"smjonas/inc-rename.nvim",
-		config = function()
-			require("inc_rename").setup()
-		end,
-	},
+  -- IncRename
+  {
+    "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup()
+    end,
+  },
 
-	-- Todo Comments
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+  -- Todo Comments
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 
-	-- Mini
-	{ "echasnovski/mini.bracketed", version = "*" },
-	{ "echasnovski/mini.indentscope", version = "*" },
-	{ "echasnovski/mini.surround", version = "*" },
+  -- Mini
+  { "echasnovski/mini.bracketed",   version = "*" },
+  { "echasnovski/mini.indentscope", version = "*" },
+  { "echasnovski/mini.surround",    version = "*" },
 
-	-- Copilot
-	"github/copilot.vim",
+  -- Copilot
+  "github/copilot.vim",
 
-	-- Formatters
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = "nvim-lua/plenary.nvim",
-	},
+  -- Formatters
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+  },
 
-	-- Home screen
-	{
-		"goolord/alpha-nvim",
-		config = function()
-			require("alpha").setup(require("alpha.themes.dashboard").config)
-		end,
-	},
+  -- Home screen
+  {
+    "goolord/alpha-nvim",
+    config = function()
+      require("alpha").setup(require("alpha.themes.dashboard").config)
+    end,
+  },
 
-	-- Comment
-	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-	},
+  -- Comment
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+  },
 
-	-- NPM packages
-	"vuki656/package-info.nvim",
+  -- NPM packages
+  "vuki656/package-info.nvim",
 
-	-- Tmux navigator
-	"christoomey/vim-tmux-navigator",
+  -- Tmux navigator
+  "christoomey/vim-tmux-navigator",
 
-	-- HardTime
-	{
-		"m4xshen/hardtime.nvim",
-		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
+  -- HardTime
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",                          -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
 
 local opts = {}
